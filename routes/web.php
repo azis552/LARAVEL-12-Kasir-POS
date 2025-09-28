3d8b5e61cf61992cca0e7b79f264e344c6818ddd
azis552 <azisaan552@gmail.com>
2025-09-28 21:56:07 +0700

    menu

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
+use App\Http\Controllers\MenusController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [userController::class, 'index'])->name('users.index');
-
 Route::get('users', [UserController::class, 'create'])->name('users.create');
-
 Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
 Route::post('users', [UserController::class, 'store'])->name('users.store');
-
 Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+
+Route::resource('menus', MenusController::class);
